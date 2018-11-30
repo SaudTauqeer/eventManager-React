2411e8286ae0c56cc51da754d8abf5964669b620
Saud Tauqeer <saudtauqeer381@gmail.com>
2018-12-01 04:45:32 +0500

    component --> timeZone --> form.js

--- src/components/TimeZone/Form.js
+++ src/components/TimeZone/Form.js
@@ -35,23 +35,47 @@ class FormInput extends Component {
 
   render(){
          return (
-          <div>
-          <Container className=" registerForm pt-5">
+
+          <header className="masthead">
+          <div className="container h-100">
+            <div className="row h-100">
+              <div className="col-lg-7 my-auto">
+                <div className="header-content mx-auto">
+
+                          <Container className=" registerForm pt-5">
           <Form className="text-white">
             <Row form>
-              <Col lg={10}>
+              <Col lg={6}>
                 <FormGroup className="pt-5">
-
-                  <Label >Event Name</Label>
-                  <Input type="text" name="event" placeholder="Enter Event name." onChange={this.handleChange} />
-
+                  <Label >Time Zone</Label>
+                  <Input type="text" name="timeZone" placeholder="i.e: Europe/Mariehamn"  onChange={this.handleChange} />
                 </FormGroup>
-                <Button className="btn btn-sm btn-success mb-4" onClick={this.onSubmit} >Submit</Button>
+                <Button className="btn btn-md btn-primary mb-4" onClick={this.onSubmit} >Submit</Button>
                 </Col>
                 </Row>
           </Form>
         </Container>
-          </div>
+        <Container>
+          <h2>
+            Please enter your time zone correctly, 
+            as it will show you correct date and time. if not, events will not be sent at given time.
+          </h2>
+          <ul> 
+            <li>
+            Please refer to the link below and copy your Time Zone under the "Time Zone" column.
+            </li>
+            <li>
+              <a target="blank" href="https://timezonedb.com/time-zones">
+                Time Zones List
+              </a>
+            </li>
+          </ul>
+        </Container>
+            </div>
+              </div>
+                </div>
+                  </div>
+        </header> 
         );
        }
     }
