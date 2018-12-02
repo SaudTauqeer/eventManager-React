b9e78612c4070931e7997e94fb5ad1e8dd0a2c13
Saud Tauqeer <saudtauqeer381@gmail.com>
2018-12-02 05:46:46 +0500

    Components --> edited Clock.js and Date.js --> fixed mem leak.

--- src/components/CreateEvent/Date.js
+++ src/components/CreateEvent/Date.js
@@ -1,22 +1,35 @@
-
+//imports
 import React, {Component} from "react";
 import "isomorphic-fetch";
 import {Container, Row, Col} from "reactstrap";
+import axios from "axios";
+
+const currentUser =  "http://localhost:3001/api/user";
 let apiKey = process.env.REACT_APP_TIME_API_KEY;
 
 class Date extends Component {
     
     
     componentWillMount(){
+        axios.get(currentUser)
+        .then(res => res.data)
+        .then(data => data.userZone[0].timeZone)
+        .then( currentUserTimeZone => this.setState({
+            fetchUrl:`http://vip.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=zone&zone=${currentUserTimeZone}`
+        }))
+
       var intervalId =  setInterval(this.fetchDate, 2000);
       this.setState({intervalId: intervalId});
     }
 
+
+
+    
     componentWillUnmount () {
         clearInterval(this.state.intervalId);
     }    
     state={
-        fetchUrl:`http://vip.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=zone&zone=Asia/Karachi`,
+        fetchUrl: null,
         year: null,
         month: null,
         day: null,
--- src/components/Events/Clock.js
+++ src/components/Events/Clock.js
@@ -2,31 +2,40 @@
 import React, {Component,} from "react";
 import "isomorphic-fetch";
 import {Container, Row, Col} from "reactstrap";
+import axios from "axios";
+
 let apiKey = process.env.REACT_APP_TIME_API_KEY;
+const currentUser =  "http://localhost:3001/api/user";
+
 
 class Clock extends Component {
 
-
     componentWillMount(){
-     var intervalId =  setInterval(this.fetchTime, 2000);
+        axios.get(currentUser)
+        .then(res => res.data)
+        .then(data => data.userZone[0].timeZone)
+        .then( currentUserTimeZone => this.setState({
+            fetchUrl:`http://vip.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=zone&zone=${currentUserTimeZone}`
+        }))
+        
+         var intervalId =  setInterval(this.fetchTime, 2000);
         //store interval id in state for cleanup
         this.setState({intervalId: intervalId});
-
     }
 
     componentWillUnmount () {
         clearInterval(this.state.intervalId);
     }
-    	
+
     state={
-        fetchUrl: `http://vip.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=zone&zone=Asia/Karachi`,
+        fetchUrl: null,
         h: null,
         m: null,
         isTimeLoaded: false,
         executeTimeHour: this.executeTimeHour,
         executeTimeMinutes:this.executeTimeMinutes,
-        intervalId : null
-
+        intervalId: null,
+        timeZone: null
 
     }
 
