65f0cf1a6304276abd19764ae0c2bc37ca171722
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-01-31 16:01:01 +0500

    Added server pinger.

--- src/components/Index/Navbar.js
+++ src/components/Index/Navbar.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem,Button } from 'reactstrap';
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button
+} from "reactstrap";
 
 const googleAuthRoute = "http://eventmanager-web-api.herokuapp.com/auth/google";
 
@@ -13,6 +21,13 @@ export default class Example extends React.Component {
     };
   }
 
+  async componentDidMount() {
+    await fetch("eventmanager-server.herokuapp.com").then(res => {
+      if (res.status === 200) {
+        console.log("Node server running");
+      }
+    });
+  }
   toggleNavbar() {
     this.setState({
       collapsed: !this.state.collapsed
@@ -20,17 +35,37 @@ export default class Example extends React.Component {
   }
   render() {
     return (
-      <div >
-        <Navbar className= "navbar navbar-expand-lg bg-dark fixed-top" dark id="mainNav">
-          <NavbarBrand className="mr-auto navbar-brand">Event Manager  </NavbarBrand>
-          <NavbarToggler className="navbar-toggler navbar-toggler-right" onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse  isOpen={!this.state.collapsed} id="navbarResponsive" navbar>
+      <div>
+        <Navbar
+          className="navbar navbar-expand-lg bg-dark fixed-top"
+          dark
+          id="mainNav"
+        >
+          <NavbarBrand className="mr-auto navbar-brand">
+            Event Manager{" "}
+          </NavbarBrand>
+          <NavbarToggler
+            className="navbar-toggler navbar-toggler-right"
+            onClick={this.toggleNavbar}
+            className="mr-2"
+          />
+          <Collapse isOpen={!this.state.collapsed} id="navbarResponsive" navbar>
             <Nav navbar className="navbar-nav ml-auto">
               <NavItem className="navbar-brand">
-              <Button className=" text-white btn btn-lg btn-success" href={googleAuthRoute}>Log In</Button>
+                <Button
+                  className=" text-white btn btn-lg btn-success"
+                  href={googleAuthRoute}
+                >
+                  Log In
+                </Button>
               </NavItem>
               <NavItem className="navbar-brand">
-              <Button className=" text-white btn btn-lg btn-info" href={googleAuthRoute}>Sign Up</Button>
+                <Button
+                  className=" text-white btn btn-lg btn-info"
+                  href={googleAuthRoute}
+                >
+                  Sign Up
+                </Button>
               </NavItem>
             </Nav>
           </Collapse>
